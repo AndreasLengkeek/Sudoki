580cee86cc432e4d2677bd00cf32a80b91549309
Andreas Lengkeek <andreas.lengkeek@gmail.com>
2023-11-12 12:10:20 +1100

    Fix new line

--- app/page.tsx
+++ app/page.tsx
@@ -2,31 +2,10 @@ import Board from './components/board';
 import fs from 'fs';
 import { Metadata } from 'next';
 
-function getStaticProps(): any[] {
-  // generate puzzles (easy): java -jar hodoku.jar /s /sl 0 /o easy.txt
-  // generate solutions: java -jar hodoku.jar /vs /bs puzzles.txt /o answers.txt
-  const puzzleFile = fs.readFileSync('./app/assets/puzzles.txt', 'utf8');
-  const solutionFile = fs.readFileSync('./app/assets/answers.txt', 'utf8');
-  const puzzles = puzzleFile.split('\n');
-  const solutions = solutionFile.split('\n');
-  const games = puzzles.map((p, i) => {
-    var row = solutions[i].split(' ');
-    return {
-      row: row[1] + ' ' + row[3],
-      puzzle: p.split(' ')[0],
-      sol: row[0],
-      diff: row[2]
-    };
-  });
-
-  console.log('loaded puzzles', games.length)
-
-  return games;
-}
 function getStaticProps2(): any[] {
   // generate solutions: java -jar hodoku.jar /vs /bs puzzles.txt /o answers.txt
   const puzzleFile = fs.readFileSync('./app/assets/puzzles.csv', 'utf8');
-  const puzzles = puzzleFile.split('\r\n');
+  const puzzles = puzzleFile.split('\n');
   const games = puzzles.map((p, i) => {
     var row = p.split(',');
     return {
